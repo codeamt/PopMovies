c147b0a0e13e64cc99083609bf785f2b7ac59106
AnnMargaret Tutu <codeamt@users.noreply.github.com>
2018-06-04 17:06:07 -0400

    Cleaned File

---
+++ app/src/main/java/com/example/annmargaret/popularmovies2/database/KeyStore.java
@@ -0,0 +1,7 @@
+
+package com.example.annmargaret.popularmovies2.database;
+
+public class KeyStore {
+    public static final String API_KEY = "YOUR_KEY";
+    public static final String YOUTUBE_API_KEY = "YOUR_KEY";
+}
