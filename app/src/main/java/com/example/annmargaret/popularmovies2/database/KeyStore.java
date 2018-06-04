37f36c1d24eb0fe49431428474e2061ed83c16ee
AnnMargaret Tutu <codeamt@users.noreply.github.com>
2018-06-04 17:05:22 -0400

    Delete KeyStore.java

--- app/src/main/java/com/example/annmargaret/popularmovies2/database/KeyStore.java
+++
@@ -1,6 +0,0 @@
-package com.example.annmargaret.popularmovies2.database;
-
-public class KeyStore {
-    public static final String API_KEY = "YOUR_KEY";
-    public static final String YOUTUBE_API_KEY = "YOUR_KEY";
-}
